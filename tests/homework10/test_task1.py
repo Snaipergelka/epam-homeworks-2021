20a44d1498295bac4ff60a4f6c63f3007c163ebd
Snaipergelka <angelinasheyko@yandex.ru>
2022-01-13 22:09:29 +0300

    fixed test case

--- tests/homework10/test_task1.py
+++ tests/homework10/test_task1.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+from unittest.mock import Mock, patch
 
 import aiohttp
 from aioresponses import aioresponses
@@ -15,30 +16,31 @@ def test_request():
     loop = asyncio.get_event_loop()
     test_instance = BusinessInsiderParser(session=aiohttp.ClientSession())
 
-    cbr_pattern = re.compile(r'^http://www.cbr.ru/scripts.*$')
-
     sp_pattern = re.compile(
         r"^https://markets.businessinsider.com/index/components/s&p_500.*$")
 
     company_pattern = re.compile(
         r"^https://markets.businessinsider.com/stocks/.*$")
 
-    with aioresponses(status=243) as m:
-        cbr_text = """<Valute ID="R01215">
-                      <NumCode>208</NumCode>
-                      <CharCode>DKK</CharCode>
-                      <Nominal>1</Nominal>
-                      <Name>Датская крона</Name>
-                      <Value>11,1939</Value>
-                      </Valute>
-                      <Valute ID="R01235">
-                      <NumCode>840</NumCode>
-                      <CharCode>USD</CharCode>
-                      <Nominal>1</Nominal>
-                      <Name>Доллар США</Name>
-                      <Value>75,1315</Value>
-                      </Valute>"""
-        m.get(cbr_pattern, body=cbr_text)
+    mock_get_patcher = patch('homework10.task1.requests.get')
+    mock_get = mock_get_patcher.start()
+    cbr_text = """<Valute ID="R01215">
+                          <NumCode>208</NumCode>
+                          <CharCode>DKK</CharCode>
+                          <Nominal>1</Nominal>
+                          <Name>Датская крона</Name>
+                          <Value>11,1939</Value>
+                          </Valute>
+                          <Valute ID="R01235">
+                          <NumCode>840</NumCode>
+                          <CharCode>USD</CharCode>
+                          <Nominal>1</Nominal>
+                          <Name>Доллар США</Name>
+                          <Value>75,1</Value>
+                          </Valute>"""
+    mock_get.return_value = Mock(status_code=200, text=cbr_text)
+
+    with aioresponses(status=200) as m:
 
         m.get(sp_pattern, body=str(
             open("tests/homework10/html.txt").read()), repeat=True)
@@ -50,10 +52,11 @@ def test_request():
             test_instance.get_load_return_all_data(
                 "https://markets.businessinsider.com/index/components/s&p_500")
         )
+    mock_get_patcher.stop()
 
     assert resp == [
         {
-            'price': 10090.070465,
+            'price': 10125.733,
             'code_name': 'ABBV',
             'p_e': 9.91,
             'income': 32.84549651311266,
@@ -61,7 +64,7 @@ def test_request():
             'one_year_result': 27.87
         },
         {
-            'price': 10090.070465,
+            'price': 10125.733,
             'code_name': 'ABBV',
             'p_e': 9.91,
             'income': 32.84549651311266,
